f3c73af11e4129f9f6b83494e35dee706dad6b21
aootoo <33767809@qq.com>
2025-05-03 13:41:06 +0800

    Update Constants.java

--- common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
+++ common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final String APP_PACKAGE_NAME = "com.xianni.app";
 
-    public static final String PROVIDER_AUTHORITY = APP_PACKAGE_NAME + ".ServiceProvider";
+    public static final String PROVIDER_AUTHORITY = com.xianni.app.ServiceProvider";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String UPDATE_URL_BASE = "https://api.nullptr.icu/android/hide-my-applist/static/";
